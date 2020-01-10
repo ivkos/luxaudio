2ddf751569e569578040dae05830d31e9d28d4ed
Ivaylo Stoyanov <me@ivkos.com>
2020-01-10 20:43:29 +0200

    luxaudio: Make 1024 the default FFT size due to snappier performance

--- src/luxaudio/utils/flags.go
+++ src/luxaudio/utils/flags.go
@@ -9,7 +9,7 @@ func GetFlags() (string, uint16, int, int, int, int, float64, float64, string, s
 	var host = flag.String("host", "", "host of the luxsrv")
 	var port = flag.Uint("port", DefaultPort, "port of the luxsrv")
 	var ledCount = flag.Int("leds", 0, "number of LEDs to be driven (max 255)")
-	var fftSize = flag.Int("fft", 2048, "FFT size")
+	var fftSize = flag.Int("fft", 1024, "FFT size")
 	var sampleRate = flag.Int("sampleRate", 0, "sample rate")
 	var channels = flag.Int("channels", 2, "number of channels")
 	var decay = flag.Float64("decay", 0.50, "decay factor [0,1] controls the smoothness of the visualization")
